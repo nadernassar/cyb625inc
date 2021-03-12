d1830339c4062432df1aa6b889ed4cd2aa51bdb8
Nader Nassar <nnassar@pace.edu>
2021-03-11 21:55:21 -0500

    pattern

--- login.php
+++ login.php
@@ -18,7 +18,29 @@ if(isset($_POST["login"]))
  }
  else
  {
-  
+  //++++++++
+  $file = 'users.dat';
+$searchfor = $_POST["user_email"];
+
+// the following line prevents the browser from parsing this as HTML.
+header('Content-Type: text/plain');
+
+// get the file contents, assuming the file to be readable (and exist)
+$contents = file_get_contents($file);
+// escape special characters in the query
+$pattern = preg_quote($searchfor, '/');
+// finalise the regular expression, matching the whole line
+//$pattern = "/^.*$pattern.*\$/m";
+// search, and store all matching occurences in $matches
+if(preg_match_all($pattern, $contents, $matches)){
+   echo "Found matches:\n";
+   echo implode("\n", $matches[0]);
+}
+else{
+   echo "No matches found";
+}
+
+  //+++++++++
   if(($_POST["user_email"]=="john_smith@gmail.com") && ($_POST["user_password"]=="CYB625!z$3cure"))
   {
     $_SESSION["user_email"] = $_POST["user_email"];
