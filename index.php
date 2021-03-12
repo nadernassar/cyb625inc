2dc7279b26b2d66393c20f431bf2b4c211d93117
Nader Nassar <nnassar@pace.edu>
2021-03-11 22:22:16 -0500

    NM

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if(!isset($_COOKIE["isadmin"]))
        if($_COOKIE["isadmin"]==1){
        
         echo '<h2 align="center"> You made it... you are an admin now</h2>';
-        echo '<a align="center" href="https://cyb625inc.azurewebsites.net/contracts/gov/detoxdrinksmarket-190326133630.pdf"> Detox Drink Strategy doc </a>';
+        
         $flag=base64_encode($_SESSION['user_email']);
         echo '<h3 align="center">Your Flag is:</br>'.$flag  . substr(md5(time()), 4, 4);'</h3>';
         
